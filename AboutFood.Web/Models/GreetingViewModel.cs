c2f7c9f9d3092e91ea3f7feb13fcb497b522eb7b
Haris Andreolas <divinedlink90@yahoo.gr>
2019-10-16 19:13:36 +0300

    add greeting page, retrieve values using the controller, not by going there directly from the view

---
+++ AboutFood.Web/Models/GreetingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AboutFood.Web.Models
+{
+    public class GreetingViewModel
+    {
+        public string Message { get; set; }
+    }
+}
